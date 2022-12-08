ac6b0203e4b1fb5877a36701d2a815335b0c6aed
tseng <s07151@ms1.hinet.net>
2022-12-08 15:03:41 +0800

    init. commit

---
+++ vscode-blink/.vscode/blink.py
@@ -0,0 +1,6 @@
+from machine import Pin
+import time
+led=Pin(1,Pin.OUT)
+while True:
+    led.toggle()
+    time.sleep(0.1)
---
+++ vscode-dht11/src/main.c
@@ -0,0 +1,32 @@
+#include <Arduino.h>
+#include <SimpleDHT.h>
+
+int pinDHT11 = 2;
+SimpleDHT11 dht11(pinDHT11);
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("============")
+  Serial.println("DHT11 test!")
+  Serial.println("============")
+  
+  byte temperature = 0;
+  byte humidity = 0;
+  int err = SimpleDHTErrSuccess;
+  if ((err = dht11.read(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
+    Serial.print("Read DHT11 failed, err="); Serial.print(SimpleDHTErrCode(err));
+    Serial.print(","); Serial.println(SimpleDHTErrDuration(err)); delay(1000);
+    return;
+  }
+  
+  Serial.print("Sample OK: ");
+  Serial.print((int)temperature); Serial.print(" *C, "); 
+  Serial.print((int)humidity); Serial.println(" H");
+  
+  // DHT11 sampling rate is 1HZ.
+  delay(1500);
+}
